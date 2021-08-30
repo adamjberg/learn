2e5fd28e78f6129b914e7e399761304bdb56c062
Adam Berg <adam@xyzdigital.com>
2021-08-30 08:54:51 -0700

    Rough router

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 Without internet on a flight August 29, 2021 9:30PM
 
 Unable to yarn install so decided to build a basic "React" without create react app
+
+Need some way to return index.html when accessing url != "/".  
--- fe/src/main.ts
+++ fe/src/main.ts
@@ -1,3 +1,25 @@
+type Component = () => HTMLElement;
+
+type RouterProps = {
+  routes: {
+    path: string;
+    component: Component;
+  }[]
+}
+
+function Router(props: RouterProps) {
+  const el = document.createElement("div");
+  const path = document.location.pathname
+
+  for (const route of props.routes) {
+    if (route.path === path) {
+      el.appendChild(route.component());
+    }
+  }
+
+  return el;
+}
+
 function Container() {
   const el = document.createElement("div");
   el.setAttribute("class", "container");
@@ -25,6 +47,47 @@ function Card(props: { img: string; title: string }) {
   return el;
 }
 
+function HomePage() {
+  const el = document.createElement("div");
+
+  const container = Container();
+
+  const cardProps = [
+    {
+      img: "/static/AdamsWorld-0.0.1.png",
+      title: "Create a React App with Create-React-App",
+    },
+    {
+      img: "/static/AdamsWorld-0.0.1.png",
+      title: "Build a Personal Finance App",
+    }
+  ];
+
+  cardProps.forEach((cardProp) => {
+    container.appendChild(
+      Card(cardProp)
+    );
+  })
+
+  el.appendChild(container)
+
+  return el;
+}
+
+function ViewPostPage() {
+  const el  = document.createElement("div");
+
+  const container = Container();
+  const h1 = document.createElement("h1");
+  h1.innerText = "View Post";
+
+  container.appendChild(h1)
+
+  el.appendChild(container)
+
+  return el;
+}
+
 function App() {
   const el = document.createElement("div");
 
@@ -47,7 +110,18 @@ function App() {
     );
   })
 
-  el.appendChild(container);
+  el.appendChild(Router({
+    routes: [
+      {
+        path: "/",
+        component: HomePage
+      },
+      {
+        path: "/posts/:id",
+        component: ViewPostPage
+      }
+    ]
+  }));
 
   return el;
 }
